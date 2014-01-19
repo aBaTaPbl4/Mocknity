69dde8f24031a8133655bf1d01c26dc5fc02f270
avatar <aBaTaPbl4@yandex.ru>
2014-01-19 20:14:36 +0400

    missed files

---
+++ Mocknity/Strategies/Structure/StubAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mocknity.Strategies.Structure
+{
+    public abstract class StubAction
+    {
+        public abstract void Execute(object mock);
+    }
+}
---
+++ Mocknity/Strategies/Structure/StubActionGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mocknity.Strategies.Structure
+{
+    public class StubActionGeneric<TMock> : StubAction
+    {
+        private readonly Action<TMock> _stubAction;
+        public StubActionGeneric(Action<TMock> stubAction)
+        {
+            _stubAction = stubAction;
+        }
+
+        public override void Execute(object mock)
+        {
+            _stubAction((TMock)mock);
+        }
+    }
+}
